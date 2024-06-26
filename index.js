9a3b18470afaf215b2dbcbe98bb5299f848eb6f0
HasanCoder66 <hasanashraf874@gmail.com>
2024-06-26 08:08:20 +0500

    server listening

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+
+
+dotenv.config();
+
+const app = express()
+const PORT = 8500
+app.listen(PORT, () => {
+    console.log(`Server Listening on ${PORT}`)
+})
